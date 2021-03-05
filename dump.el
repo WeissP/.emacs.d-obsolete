d2b982b73aee5d5ec171a656980bfb8f88f9bcd7
weiss <kingbaiing@163.com>
2021-03-05 13:03:14 +0100

    update dump

--- dump.el
+++ dump.el
@@ -4,11 +4,13 @@
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
 
-(load "/home/weiss/.emacs.d/config/init/weiss-startup.el")
+(load "/home/weiss/.emacs.d/configs/weiss_startup.el")
 
 (load-theme 'doom-one-light t t)
 
 (load "/home/weiss/.emacs.d/recentf")
+
+(load "/home/weiss/.emacs.d/dumped-packages.el")
 
 ;; We have to unload tramp in pdump, otherwise tramp will not work.
 (tramp-unload-tramp)
