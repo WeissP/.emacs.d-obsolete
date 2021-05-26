c909c7d26715165c2f9936fa88a90946f3317d8a
weiss <kingbaiing@163.com>
2021-05-26 16:10:05 +0200

    update abbrevs

--- configs/weiss_html<abbrevs.el
+++ configs/weiss_html<abbrevs.el
@@ -1,43 +1,45 @@
+(setq weiss-html-mode-abbrev-table
+      '(
+        ("b" "<b> ▮ </b>")
+        ("i" "<i> ▮ </i>")
+        ("p" "<p> ▮ </p>")
+        ("a" "<a href=\"▮\"> </a>" weiss--ahf)
+
+        ("h1" "<h1> ▮ </h1>")
+        ("h2" "<h2> ▮ </h2>")
+        ("h3" "<h3> ▮ </h3>")
+        ("h4" "<h4> ▮ </h4>")
+        ("li" "<li> ▮ </li>")
+        ("ol" "<ol start=1 type=a> \n▮ \n </ol>" weiss--ahf-indent)
+        ("ul" "<ul> \n▮ \n </ul>" weiss--ahf-indent)
+        ("hd" "<head>\n<meta charset=\"utf-8\">\n<title>▮</title>\n</head>" weiss--ahf-indent)
+        ("th" "<th>▮</th>" weiss--ahf)
+        ("br" "<br>" weiss--ahf)
+        ("tr" "<tr>\n▮\n</tr>" weiss--ahf-indent)
+        ("td" "<td>▮</td>" weiss--ahf)
+        ("sp" "<span>▮</span>" weiss--ahf)
+        ("sb" "<span class=\"block\">▮</span>" weiss--ahf)    
+        ("ag" "Aufgabe" weiss--ahf)
+
+        ("el" "&lt;" weiss--ahf)
+        ("eg" "&gt;" weiss--ahf)
+        ("elg" "&lt;▮&gt;" weiss--ahf)
+        ("ea" "&amp" weiss--ahf)
+        ("eq" "&quot;" weiss--ahf)
+
+        ("pre" "<pre>\n▮\n</pre>" weiss--ahf-indent)    
+        ("php" "<?php\n▮\n?>" weiss--ahf)
+        ("div" "<div> ▮ </div>")
+
+        ("code" "<pre>\n<code>\n▮\n</code>\n</pre>" weiss--ahf-indent)
+        ("style" "<style>\n▮\n</style>" weiss--ahf-indent)
+        ("temp" "<!doctype html>\n<html lang=\"de\">\n\n<head>\n<meta charset=\"utf-8\">\n<title>▮</title>\n</head>\n\n<body>\n\n</body>" weiss--ahf-indent)
+
+        ))
+
 (when (boundp 'html-mode-abbrev-table)
   (clear-abbrev-table html-mode-abbrev-table))
 
-(define-abbrev-table 'html-mode-abbrev-table
-  '(
-    ("b" "<b> ▮ </b>")
-    ("i" "<i> ▮ </i>")
-    ("p" "<p> ▮ </p>")
-    ("a" "<a href=\"▮\"> </a>" weiss--ahf)
-
-    ("h1" "<h1> ▮ </h1>")
-    ("h2" "<h2> ▮ </h2>")
-    ("h3" "<h3> ▮ </h3>")
-    ("h4" "<h4> ▮ </h4>")
-    ("li" "<li> ▮ </li>")
-    ("ol" "<ol start=1 type=a> \n▮ \n </ol>" weiss--ahf-indent)
-    ("ul" "<ul> \n▮ \n </ul>" weiss--ahf-indent)
-    ("hd" "<head>\n<meta charset=\"utf-8\">\n<title>▮</title>\n</head>" weiss--ahf-indent)
-    ("th" "<th>▮</th>" weiss--ahf)
-    ("br" "<br>" weiss--ahf)
-    ("tr" "<tr>\n▮\n</tr>" weiss--ahf-indent)
-    ("td" "<td>▮</td>" weiss--ahf)
-    ("sp" "<span>▮</span>" weiss--ahf)
-    ("sb" "<span class=\"block\">▮</span>" weiss--ahf)    
-    ("ag" "Aufgabe" weiss--ahf)
-
-    ("el" "&lt;" weiss--ahf)
-    ("eg" "&gt;" weiss--ahf)
-    ("elg" "&lt;▮&gt;" weiss--ahf)
-    ("ea" "&amp" weiss--ahf)
-    ("eq" "&quot;" weiss--ahf)
-
-    ("pre" "<pre>\n▮\n</pre>" weiss--ahf-indent)    
-    ("php" "<?php\n▮\n?>" weiss--ahf)
-    ("div" "<div> ▮ </div>")
-
-    ("code" "<pre>\n<code>\n▮\n</code>\n</pre>" weiss--ahf-indent)
-    ("style" "<style>\n▮\n</style>" weiss--ahf-indent)
-    ("temp" "<!doctype html>\n<html lang=\"de\">\n\n<head>\n<meta charset=\"utf-8\">\n<title>▮</title>\n</head>\n\n<body>\n\n</body>" weiss--ahf-indent)
-
-    ))
+(define-abbrev-table 'html-mode-abbrev-table weiss-html-mode-abbrev-table)
 
 (provide 'weiss_html<abbrevs)
--- configs/weiss_php<abbrevs.el
+++ configs/weiss_php<abbrevs.el
@@ -1,12 +1,30 @@
-(when (boundp 'php-mode-abbrev-table)
-  (clear-abbrev-table php-mode-abbrev-table))
+(with-eval-after-load 'weiss_html<abbrevs
+  (when (boundp 'php-mode-abbrev-table)
+    (clear-abbrev-table php-mode-abbrev-table))
 
-(define-abbrev-table 'php-mode-abbrev-table
-  '(
-    ("php" "<?php\n▮\n?>" weiss--ahf)
-    ("e" "echo ▮;" weiss--ahf)
-    ("rt" "return ▮;" weiss--ahf)
-    ("g" "global ▮;" weiss--ahf)
-    ))
+  (define-abbrev-table 'php-mode-abbrev-table
+    (append weiss-html-mode-abbrev-table
+            '(
+              ("php" "<?php\n▮\n?>" weiss--ahf)
+              ("e" "echo ▮;" weiss--ahf)
+              ("rt" "return ▮;" weiss--ahf)
+              ("g" "global ▮;" weiss--ahf)
+              )
+            )
+    )
+
+  (define-abbrev-table 'web-mode-abbrev-table
+    (append weiss-html-mode-abbrev-table
+            '(
+              ("php" "<?php\n▮\n?>" weiss--ahf)
+              ("e" "echo ▮;" weiss--ahf)
+              ("rt" "return ▮;" weiss--ahf)
+              ("g" "global ▮;" weiss--ahf)
+              )
+            )
+    )
+  )
+
+
 
 (provide 'weiss_php<abbrevs)
